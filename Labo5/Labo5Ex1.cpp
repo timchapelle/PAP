554b25343edc23b7bd8599714249895be3078fa9
timchapelle <horace22@gmail.com>
2016-01-20 11:15:47 +0100

    Bug Ex3 corrigé + Ex6

--- Labo5/Labo5Ex1.cpp
+++ Labo5/Labo5Ex1.cpp
@@ -1,3 +1,16 @@
+/*EXO1 Faire la table de multiplication d’un entier lu au clavier entre 1 et 20 obligatoirement.
+Faire les calculs de 1 à 10 fois. Puis, demandez à l’utilisateur s’il veut recommencer une autre table.
+Ex : Table de quel entier entre 1 et 20 : 34
+Cette valeur n’est pas entre 1 et 20, recommencez : -2
+Cette valeur n’est pas entre 1 et 20, recommencez : 3
+Table de 3
+1 X 3 = 3
+. . .
+10 X 3 = 30
+Voulez-vous faire une autre table ( - O(ui) - N(on) - ) t
+Répondez O(ui) ou N(on) J
+Répondez O(ui) ou N(on) O */
+
 #include <iostream>
 using namespace std;
 
@@ -13,7 +26,7 @@ while (choix == 'o' || choix == 'O')
 	cin >> nb;
 	   } while (nb <= 1 || nb >= 20) ;
 	  
-	  for (int i = 0; i <= 10; i++)
+	  for (int i = 1; i <= 10; i++)
 	  {
 		  cout << i << "*" << nb << " = " << i * nb << endl;
 	  }
--- Labo5/Labo5Ex3.cpp
+++ Labo5/Labo5Ex3.cpp
@@ -1,3 +1,25 @@
+/*EXO3 Dans un magasin, on fait une facture par client pour différents clients. L’encodage des clients
+se terminent lorsqu’on introduit * pour son nom.
+Pour chaque facture, on introduit des produits (par produit : description, prix unitaire, quantité
+ achetée). L’encodage des produits se terminent lorsqu’on entre * pour sa description.
+On demande alors si le client a une carte de membre. Si oui, il aura 10% de remise.
+Ex : Nom du client : DUPONT
+Produit : coca
+Prix : 1.15
+Quantité : 4
+Prix total de l’article coca = 4.6 €
+Produit : choco
+Prix : 2.2
+Quantité : 3
+Prix total de l’article choco = 6.6 €
+Produit : *
+Prix total : 11.2 €
+A-t’ il une carte de fidélité ( - O(ui) – N(on) - ) Z
+O(ui) – N(on) SVP ! o
+Remise = 1.12 €
+Montant à payer de DUPONT = 10.08 €
+Nom du client :  									*/
+
 #include <iostream>
 using namespace std;
 
@@ -8,12 +30,13 @@ string client=" ",produit=" ";
 double prix,prixTotal,remise,total;;
 int qt;
 
-cout << "Nom du client :"; cin >> client;
 while (client != "*")
-{
+{	
+	cout << "Nom du client :"; cin >> client;
+	cout << "Produit : "; cin >> produit;
+	
 	while (produit != "*")
-	{
-		cout << "Produit : "; cin >> produit;
+	{	remise=0;
 		cout << "Prix : "; cin >> prix;
 		cout << "Quantité : "; cin >> qt;
 		
@@ -38,7 +61,6 @@ while (client != "*")
 		cout << "Montant à payer de " << client << " : " << total << " €" << endl;
 		cout << "Produit : "; cin >> produit;
 	} // fin de la boucle while produit != *
-	cout << "Client : "; cin >> client;
-} // fin de la boucle si client != *
+} // fin de la boucle while client != *
 return 0; // si client = *, on quitte le programme
 } // fin du main
