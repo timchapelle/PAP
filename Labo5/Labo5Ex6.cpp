c5b81aecaca14fb712cfc443ddb6956a2b73da5f
timchapelle <horace22@gmail.com>
2016-01-20 01:11:09 +0100

    ajout exo 5 et 6

---
+++ Labo5/Labo5Ex6.cpp
@@ -0,0 +1,13 @@
+/* LABO 5 - EXO6 Calculer le PGCD de 2 entiers selon la méthode d’Euclide.
+Le calcul du PGCD n’est possible que pour des entiers > 1.
+Méthode d’Euclide :
+- Diviser le plus grand par le plus petit.
+- Si le reste n’est pas 0, diviser le diviseur par le reste. (Et toujours, ainsi)
+- Si le reste = 0, le PGCD est le diviseur. */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+}
