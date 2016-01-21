e2661974bae782e5f1680466ac85747382fde071
timchapelle <horace22@gmail.com>
2016-01-21 16:34:02 +0100

    correction ex. 7

--- Labo5/Labo5Ex7.cpp
+++ Labo5/Labo5Ex7.cpp
@@ -8,17 +8,23 @@
 using namespace std;
 
 int pgcd (int a, int b) {       // Fonction pgcd
-	int tmp;
-	if ( b > a )
-		{tmp=a;a=b;b=tmp;}
-	int reste;
-	while ( reste != 0 )
-	{
-		reste=a % b;
-		a=b;
-		b=reste;
-	}
-return b;
+
+int resultat;
+bool ok=false;
+
+while ( ok != true ) 
+{
+	if (a > b) 
+	   { a = a-b; }
+	 else if (a < b) 
+	   { b = b-a; }
+	 else 
+	 {
+		resultat = a;
+		ok = true;
+	 }
+}
+return resultat;
 }
 
 int main() {
@@ -29,19 +35,7 @@ while (x <= 1 || y <= 1 )
 { cout << "Entrez 2 entiers supérieurs à 1" << endl;
 	cin >> x >> y ; }
 	
-if (x == y)
-{
-	resultat=x;
-}
-else if (x > y)
-{
-	resultat=pgcd((x-y),y);
-}
-else if (x < y)
-{
-	resultat=pgcd(x,(y-x));
-}
+resultat=pgcd(x,y);
 
-cout << "Résultat : " << resultat ;
-
+cout << "PGCD : " << resultat;
 }
