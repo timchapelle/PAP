f3b6679453b6c5405fd94406a1e51df38c21e9d3
timchapelle <horace22@gmail.com>
2016-02-02 19:34:18 +0100

    indentation

--- Labo5/Labo5Ex5.cpp
+++ Labo5/Labo5Ex5.cpp
@@ -8,38 +8,26 @@ Ex : Pour 10 et 14, il affichera 36
 using namespace std;
 
 int main() {
-
 	int nb1, nb2, somme=0;
-
 	cout << "Entrez 2 entiers : " << endl;
 	cin >> nb1 >> nb2;
-
-	if ( nb1 < nb2 )
-	{
-		for (int i = nb1; i <= nb2; i++)
-		{
-			if ((i%2)==0)
-			{ 
+	if ( nb1 < nb2 ){
+		for (int i = nb1; i <= nb2; i++){
+			if ((i%2)==0){ 
 				somme=somme+i;
 			}
-			else 
-			{
+			else {
 				cout << "impair";
 			}
 		} // fin de for
 	} // fin de if nb1 < nb2
-
-	else if ( nb1 > nb2 ) 
-	{
-		for (int i = nb2; i <= nb1; i++)
-		{
-			if ((i%2)==0) 
-			{
+	else if ( nb1 > nb2 ) {
+		for (int i = nb2; i <= nb1; i++){
+			if ((i%2)==0) {
 				somme=somme+i;
 			}
 		} // fin de for
 	} // fin de if nb1 > nb2
-
 	cout << "Somme : " << somme ;
 	return 0;
 }
